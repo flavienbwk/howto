f6ca061c674dc32e0bf75d7a71c919b2a30f2ab2
flavienbwk <flavien@berwick.fr>
2021-04-23 22:54:15 +0200

    Added initial architectures

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # howto
+
 The howto command line allows you to design or play tutorials, guides or even stories in your shell.
+
+## Install
+
+```bash
+pip3 install howto-cli
+```
+
+## Usage
+
+```bash
+howto 
+```
+
+## Creating a scenario
---
+++ cli/howto/__main__.py
@@ -0,0 +1,61 @@
+import howto
+import sys
+import warnings
+import argparse
+import pkg_resources
+
+import logging
+from logging import DEBUG, INFO, WARNING
+
+logger = logging.getLogger("HOWTO")
+
+
+def parse_options(args=None, values=None):
+    """
+    Define and parse `optparse` options for command-line usage.
+    """
+    parser = argparse.ArgumentParser(
+        description="Multi-scenarios CLI tool for tutorials, guides or stories. More help at : https://github.com/flavienbwk/howto."
+    )
+    parser.add_argument("f", type=str, help="JSON scenario file path")
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_const",
+        const=DEBUG,
+        dest="verbosity",
+        default=INFO,
+        help="Print all operations.",
+    )
+    args = parser.parse_args()
+
+    parameters = {"scenario_path": args.f}
+
+    return parameters, args.verbosity
+
+
+def run():
+    """Run howto from the command line."""
+    version = pkg_resources.require("howto-cli")[0].version
+    logger.info(f"Welcome to howto {version} !")
+
+    # Parse options and adjust logging level if necessary
+    options, logging_level = parse_options()
+    if not options:
+        sys.exit(2)
+    logger.setLevel(logging_level)
+    console_handler = logging.StreamHandler()
+    logger.addHandler(console_handler)
+    if logging_level <= WARNING:
+        # Ensure deprecation warnings get displayed
+        warnings.filterwarnings("default")
+        logging.captureWarnings(True)
+        warn_logger = logging.getLogger("py.warnings")
+        warn_logger.addHandler(console_handler)
+
+    # Run
+    howto.howtoFromFile(**options)
+
+
+if __name__ == "__main__":
+    run()
